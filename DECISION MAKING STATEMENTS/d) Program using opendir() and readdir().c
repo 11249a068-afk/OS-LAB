69d353c0e45752bce7e9962cbac5c7bdfe73f9eb
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2026-04-20 19:41:58 +0530

    Create d) Program using opendir() and readdir().c

---
+++ DECISION MAKING STATEMENTS/d) Program using opendir() and readdir().c
@@ -0,0 +1,15 @@
+Source Code:
+#include <stdio.h>
+#include <dirent.h>
+int main() {
+    DIR *dir;
+    struct dirent *entry;
+    dir = opendir(".");
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+    closedir(dir);
+    return 0;
+}
+
+
