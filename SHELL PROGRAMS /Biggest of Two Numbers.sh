975d273f50dab303ad97b139a609604bc28ff26f
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2026-04-20 19:26:00 +0530

    Create Biggest of Two Numbers.sh

---
+++ SHELL PROGRAMS /Biggest of Two Numbers.sh
@@ -0,0 +1,14 @@
+                                                      --Shell Program to Find the Biggest of Two Numbers--
+
+⏲ Program:
+#!/bin/bash
+echo "Enter first number:"
+read a
+echo "Enter second number:"
+read b
+if [ $a -gt $b ]
+then
+  echo "Biggest number is $a"
+else
+  echo "Biggest number is $b"
+fi
