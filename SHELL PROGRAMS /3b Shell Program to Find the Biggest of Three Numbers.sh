3ed3b677e952227bdc71b0ea3fd0700d36629502
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2026-04-20 19:28:25 +0530

    Create 3b Shell Program to Find the Biggest of Three Numbers.sh

---
+++ SHELL PROGRAMS /3b Shell Program to Find the Biggest of Three Numbers.sh
@@ -0,0 +1,17 @@
+                                                                            --Shell Program to Find the Biggest of Three Numbers--
+program:
+#!/bin/bash
+echo "Enter three numbers:"
+read a
+read b
+read c
+
+if [ $a -gt $b ] && [ $a -gt $c ]
+then
+  echo "Biggest number is $a"
+elif [ $b -gt $c ]
+then
+  echo "Biggest number is $b"
+else
+  echo "Biggest number is $c"
+fi
